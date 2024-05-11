2e81103d95c1ded36ec830770eeec62985e91cec
Relativiteit <alejocain@gmail.com>
2024-05-11 23:30:02 +0200

    cargo command

--- formatted.rs
+++ formatted.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("{} days", 31)
+    // println!("{} days", 31)
+    // named arguments positional
+    println!(
+        "{subject} {verb} {object}",
+        object = "the lazy dog",
+        subject = "the quick brown fox",
+        verb = "jumps over"
+    );
 }
